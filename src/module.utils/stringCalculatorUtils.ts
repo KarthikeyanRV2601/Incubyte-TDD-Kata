95ab85e87f168c70af5e60ce80cf68613c0dab51
Karthikeyan <karthikeyan.r.v.2601@gmail.com>
2024-11-23 02:07:10 +0530

    feat(v0.0.6): passing test with functionality enhancement(should return the sum of two comma-separated numbers)

--- src/module.utils/stringCalculatorUtils.ts
+++ src/module.utils/stringCalculatorUtils.ts
@@ -1,5 +1,13 @@
 export function add(numbers: string): number {
     // Now we are handling empty string
     if (numbers === "") return 0;
-    else return parseInt(numbers);
+
+    // Now we are handling single number
+    if (!numbers.includes(',')) return parseInt(numbers);
+
+    //Now we are handling the comma seperated numbers
+    return numbers
+        .split(",")
+        .map(Number)
+        .reduce((sum, num) => sum + num, 0);
 }
