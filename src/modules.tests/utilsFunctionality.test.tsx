92d31f72bbb141ec80c9ac36265b1e95cd6d0d40
Karthikeyan <karthikeyan.r.v.2601@gmail.com>
2024-11-23 01:59:51 +0530

    feat(v0.0.4): failing test(should return the number itself if only one number is provided)

--- src/modules.tests/utilsFunctionality.test.tsx
+++ src/modules.tests/utilsFunctionality.test.tsx
@@ -1,8 +1,15 @@
 import { add } from "../module.utils/stringCalculatorUtils";
 
 describe("String Calculator", () => {
+
   test("should return 0 for an empty string", () => {
     const numbers = ""
     expect(add(numbers)).toBe(0);
   });
+
+  test("should return the number itself if only one number is provided", () => {
+    const numbers = "5"
+    expect(add(numbers)).toBe(5);
+  });
+
 });
